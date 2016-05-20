a6cda39cb1229921d360ae0908f37c5a470624e6
Michael Lesniak <mlesniak@micromata.de>
2016-05-20 23:33:36 +0200

    Add logging framework

--- src/main/java/com/mlesniak/raytracer/Main.java
+++ src/main/java/com/mlesniak/raytracer/Main.java
@@ -1,10 +1,15 @@
 package com.mlesniak.raytracer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Application entry point.
  */
 public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LOG.info("Application starting");
     }
 }
