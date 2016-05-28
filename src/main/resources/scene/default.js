4aaa4c59f3abd4dc101495fdfc7622135333d19a
Michael Lesniak <mlesniak@micromata.de>
2016-05-28 21:35:56 +0200

    Additional animation example

--- src/main/resources/scene/default.js
+++ src/main/resources/scene/default.js
@@ -9,5 +9,9 @@ if (tick > ticks / 2) {
 var xTickStep = 0.1;
 var yTickStep = 0.2;
 
+var radiusStep = 0.05;
+
+scene.objects[2].radius = scene.objects[2].radius + radiusStep * direction;
+
 scene.lights[0].y = scene.lights[0].y + yTickStep * direction;
 scene.lights[0].x = scene.lights[0].x + xTickStep * direction;
